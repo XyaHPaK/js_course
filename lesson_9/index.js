e8e2181ed067a5ce3405dacd15fe2330f72a8c14
XyaHPaK <xyahpak@gmail.com>
2018-09-17 14:40:21 +0300

    Add task

---
+++ lesson_9/index.js
@@ -0,0 +1,15 @@
+'use strict'
+console.log("Карнаух Сергей");
+const a=1;
+const b=2;
+console.log(a>b?a:b);
+console.log(a<b?a:b);
+console.log(a+b);
+console.log(a%b);
+if (a-10>b){
+	console.log(a+10)
+	}else if (a<b) {
+		console.log(9)
+		} else{
+			console.log(b)
+		};
