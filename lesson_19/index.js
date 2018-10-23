2afcb0b5ed9fa9d657c582d1ec112b859866b63f
XyaHPaK <xyahpak@gmail.com>
2018-10-23 15:07:21 +0300

    Update

--- lesson_19/index.js
+++ lesson_19/index.js
@@ -1,3 +1,4 @@
+// task
 function Human(name, age){
     this.name = name;
     this.age = age;
@@ -15,3 +16,38 @@ AlevelStudent.prototype.averageMark = function(){
     }
     return marksSum/this.marks.length;
 };
+// timer
+function Timer (countNum){
+    this.count = document.getElementById('count');
+    this.seconds = 0;
+    this.numOfNum = countNum;
+    this.start = function (){
+        this.id = setInterval(() => {
+			const index = this.seconds % (this.numOfNum * 2);
+			if (index < this.numOfNum) {
+				this.items[index].style.background = 'green';
+			} else if (this.items[this.numOfNum * 2 - index - 1]) {
+				this.items[this.numOfNum * 2 - index - 1].style.background = 'black';
+			}
+
+            this.count.innerText = `${++this.seconds} seconds`;
+        }, 1000);
+    }
+    this.stop = () => clearInterval(this.id);
+    this.items = [];
+
+    for (let i = 0; i < this.numOfNum; i += 1) {
+        const item = document.createElement('span');
+
+        item.innerText = i + 1;
+
+        this.a.appendChild(item);
+        this.items.push(item);
+
+        if (i < this.numOfNum - 1) {
+            const coma = document.createElement('span');
+            coma.innerText = ', ';
+            this.a.appendChild(coma);
+        }
+    }
+}
