4b9bfc98bc86eb20ce53efa78dec4bcf036f15b3
XyaHPaK <xyahpak@gmail.com>
2018-12-19 17:57:59 +0200

    del

--- module 3/card_data.js
+++
@@ -1,9 +0,0 @@
-export default class CardData{
-    constructor(){
-        
-    }
-    getCardInfo(cn){
-       return fetch (`https://api.bincodes.com/cc/?format=json&api_key=d96ca493f5be297f8c304a87edcdf6a8&cc=${this.cn}`)
-        .then (res =>res.json())
-    }
-}
